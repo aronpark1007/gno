ef38d8bfb0b533fce72c95ec9860f03a75d575af
Aron Park <arondev1007@gmail.com>
2026-05-11 15:48:13 +0900

    test(amino): add pubkey encoding example

--- tm2/pkg/amino/example_test.go
+++ tm2/pkg/amino/example_test.go
@@ -15,10 +15,13 @@
 package amino_test
 
 import (
+	"encoding/hex"
 	"fmt"
 	"reflect"
 
 	amino "github.com/gnolang/gno/tm2/pkg/amino"
+	"github.com/gnolang/gno/tm2/pkg/crypto"
+	"github.com/gnolang/gno/tm2/pkg/crypto/ed25519"
 )
 
 func Example() {
@@ -66,4 +69,78 @@ func Example() {
 	// Encoded: 210A152F616D696E6F5F746573742E62634D65737361676512080A0341424310C801 (err: <nil>)
 	// Decoded: &{ABC 100} (err: <nil>)
 	// Decoded successfully: true
+}
+
+func Example_cryptoPubKeyEncoding() {
+	var raw [ed25519.PubKeyEd25519Size]byte
+	for i := range raw {
+		raw[i] = byte(i + 1)
+	}
+
+	pubKey := ed25519.PubKeyEd25519(raw)
+	votingPower := int64(10)
+
+	// This is the same anonymous struct shape used by Validator.Bytes().
+	validatorHashInput := struct {
+		PubKey      crypto.PubKey
+		VotingPower int64
+	}{
+		pubKey,
+		votingPower,
+	}
+
+	concretePubKey := amino.MustMarshal(pubKey)
+	anyPubKey := amino.MustMarshalAny(pubKey)
+	validatorBytes := amino.MustMarshal(validatorHashInput)
+
+	fmt.Printf("type URL: %s\n", amino.GetTypeURL(pubKey))
+	fmt.Printf("raw pubkey: %s\n", hex.EncodeToString(pubKey[:]))
+	fmt.Printf("concrete PubKeyEd25519: %s\n", hex.EncodeToString(concretePubKey))
+	fmt.Printf("crypto.PubKey as Any: %s\n", hex.EncodeToString(anyPubKey))
+	fmt.Printf("validator hash input: %s\n", hex.EncodeToString(validatorBytes))
+	fmt.Println()
+	fmt.Println("field key = (field_number << 3) | wire_type")
+	fmt.Println("wire types: varint = 0, bytes = 2")
+	fmt.Println()
+	fmt.Println("concrete PubKeyEd25519:")
+	fmt.Println("  0a = field 1, bytes")
+	fmt.Println("  20 = length 32")
+	fmt.Println("  <32 raw pubkey bytes>")
+	fmt.Println()
+	fmt.Println("crypto.PubKey Any wrapper:")
+	fmt.Println("  0a 11 = Any field 1 type_url, length 17")
+	fmt.Println("  2f746d2e5075624b657945643235353139 = /tm.PubKeyEd25519")
+	fmt.Println("  12 22 = Any field 2 value, length 34")
+	fmt.Println("  0a 20 <32 raw pubkey bytes> = concrete value")
+	fmt.Println()
+	fmt.Println("Validator.Bytes() hash input:")
+	fmt.Println("  0a 37 = outer field 1 PubKey, length 55")
+	fmt.Println("  <Any wrapper bytes>")
+	fmt.Println("  10 14 = outer field 2 VotingPower, signed varint 10")
+
+	// Output:
+	// type URL: /tm.PubKeyEd25519
+	// raw pubkey: 0102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f20
+	// concrete PubKeyEd25519: 0a200102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f20
+	// crypto.PubKey as Any: 0a112f746d2e5075624b65794564323535313912220a200102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f20
+	// validator hash input: 0a370a112f746d2e5075624b65794564323535313912220a200102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f201014
+	//
+	// field key = (field_number << 3) | wire_type
+	// wire types: varint = 0, bytes = 2
+	//
+	// concrete PubKeyEd25519:
+	//   0a = field 1, bytes
+	//   20 = length 32
+	//   <32 raw pubkey bytes>
+	//
+	// crypto.PubKey Any wrapper:
+	//   0a 11 = Any field 1 type_url, length 17
+	//   2f746d2e5075624b657945643235353139 = /tm.PubKeyEd25519
+	//   12 22 = Any field 2 value, length 34
+	//   0a 20 <32 raw pubkey bytes> = concrete value
+	//
+	// Validator.Bytes() hash input:
+	//   0a 37 = outer field 1 PubKey, length 55
+	//   <Any wrapper bytes>
+	//   10 14 = outer field 2 VotingPower, signed varint 10
 }
